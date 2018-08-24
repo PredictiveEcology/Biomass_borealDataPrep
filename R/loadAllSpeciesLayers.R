c87403be7c5d2fc318499acd31c7c30dbdda907b
Unknown <ceresvbarros@hotmail.com>
2018-08-23 17:46:53 -0700

    merge corrections

--- R/loadAllSpeciesLayers.R
+++ R/loadAllSpeciesLayers.R
@@ -9,10 +9,9 @@ loadAllSpeciesLayers <- function(dataPath, biomassMap, shpStudyRegionFull, modul
   suffix <- paste0("_", suffix)
   for (sp in speciesnamesRaw) {
     targetFile <- paste0("NFI_MODIS250m_kNN_Species_", sp, "_v0.tif")
-    filename2 <- .suffix(targetFile, suffix = suffix)
-    species1[[sp]] <- Cache(prepInputs,
+    postProcessedFilename <- .suffix(targetFile, suffix = suffix)
+    species1[[sp]] <- prepInputs(
       targetFile = targetFile,
-      alsoExtract = NA,
       archive = asPath(c("kNN-Species.tar", paste0("NFI_MODIS250m_kNN_Species_", sp, "_v0.zip"))),
       #alsoExtract = if (sp == speciesnamesRaw[1]) paste0("NFI_MODIS250m_kNN_Species_", speciesnamesRaw[-1], "_v0.tif"),
       destinationPath = asPath(dataPath),
@@ -25,14 +24,14 @@ loadAllSpeciesLayers <- function(dataPath, biomassMap, shpStudyRegionFull, modul
     )
   }
 
-  # lump Pinus banksiana and Pinus contorta together as Pinu_sp
   sumSpecies <- c("Pinu_Ban", "Pinu_Con")
   newLayerName <- grep("Pinu", speciesNamesEnd, value = TRUE)
   fname <- .suffix(file.path(dataPath, "KNNPinu_sp.tif"), suffix)
   a <- Cache(sumRastersBySpecies,
              species1[sumSpecies], newLayerName = newLayerName,
-             filenameToSave = asPath(fname))
-  a <- raster(fname) ## ensure a gets a filename -- file already exists, just need it to be part of RAM object
+             filenameToSave = asPath(fname),
+             ...)
+  a <- raster(fname) ## ensure a gets a filename
   species1[sumSpecies] <- NULL
   species1[[newLayerName]] <- a
   names(species1)[grep("Abie", names(species1))] <- grep("Abie", speciesNamesEnd, value = TRUE)
