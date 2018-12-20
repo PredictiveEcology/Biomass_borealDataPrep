7aeb30d1eb056a1a850ff2dc08cac24f24ebb9ec
Alex Chubaty <alex.chubaty@gmail.com>
2018-12-20 11:23:50 -0700

    use my fri email

--- Boreal_LBMRDataPrep.R
+++ Boreal_LBMRDataPrep.R
@@ -6,7 +6,7 @@ defineModule(sim, list(
     person("Yong", "Luo", email = "yong.luo@canada.ca", role = c("aut")),
     person(c("Eliot", "J", "B"), "McIntire", email = "eliot.mcintire@canada.ca", role = c("aut", "cre")),
     person(c("Ceres"), "Barros", email = "cbarros@mail.ubc.ca", role = c("ctb")),
-    person(c("Alex", "M"), "Chubaty", email = "alex.chubaty@gmail.com", role = c("ctb"))
+    person(c("Alex", "M."), "Chubaty", email = "achubaty@friresearch.ca", role = c("ctb"))
   ),
   childModules = character(0),
   version = numeric_version("1.3.3"),
