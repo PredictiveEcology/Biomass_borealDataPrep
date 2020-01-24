94a27cee836fbe53429d15e52e2f772b4526ac15
Tati Micheletti <tati.micheletti@gmail.com>
2020-01-23 21:38:26 -0800

    Trying to fix error

--- Biomass_borealDataPrep.R
+++ Biomass_borealDataPrep.R
@@ -790,7 +790,7 @@ Save <- function(sim) {
     standAgeMapFilename <- file.path(dPath, "NFI_MODIS250m_kNN_Structure_Stand_Age_v0.tif")
 
     sim$standAgeMap <- Cache(prepInputs,
-                             targetFile = standAgeMapFilename,
+                             # targetFile = standAgeMapFilename,
                              destinationPath = dPath,
                              url = extractURL("standAgeMap"),
                              fun = "raster::raster",
