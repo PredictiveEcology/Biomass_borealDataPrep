5b444b99bccce756353a967e292d058570c1eb2e
CeresBarros <cbarros@mail.ubc.ca>
2020-03-31 10:07:17 -0700

    mv partitionBiomass to separate script

--- R/coverOptimFn.R
+++ R/coverOptimFn.R
@@ -29,15 +29,3 @@ coverOptimFn <- function(x, pixelCohortData, subset, bm, returnRsq = TRUE) {
   else
     list(modelBiomass1 = modelBiomass1, pixelCohortData = pixelCohortData)
 }
-
-partitionBiomass <- function(x, pixelCohortData) {
-  if (!"decid" %in% colnames(pixelCohortData)) {
-    pixelCohortData[, decid := speciesCode %in% c("Popu_Tre", "Betu_Pap")]
-  }
-
-  pixelCohortData[, cover2 := cover * c(1,x)[decid + 1]]
-  pixelCohortData[, cover2 := cover2/sum(cover2), by = "pixelIndex"]
-  pixelCohortData[, B := totalBiomass*cover2]
-  pixelCohortData
-
-}
