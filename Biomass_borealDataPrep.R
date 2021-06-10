dae4aff9a2ea1775f4e0624d438eff86522d7355
CeresBarros <cbarros@mail.ubc.ca>
2021-06-09 17:30:35 -0700

    minor

--- Biomass_borealDataPrep.R
+++ Biomass_borealDataPrep.R
@@ -1341,12 +1341,12 @@ Save <- function(sim) {
 
 
 plotFn_speciesEcoregion <- function(stk, SEtype) {
-        ggSpeciesEcoregion <-
-          gplot(stk, maxpixels = 2e6) +
-          geom_tile(aes(fill = value)) +
-          facet_wrap(~ variable) +
-          scale_fill_gradient(low = 'light grey', high = 'blue', na.value = "white") +
-          theme_bw() +
-          coord_equal() +
-          ggtitle(SEtype)
-      }
+  ggSpeciesEcoregion <-
+    gplot(stk, maxpixels = 2e6) +
+    geom_tile(aes(fill = value)) +
+    facet_wrap(~ variable) +
+    scale_fill_gradient(low = 'light grey', high = 'blue', na.value = "white") +
+    theme_bw() +
+    coord_equal() +
+    ggtitle(SEtype)
+}
