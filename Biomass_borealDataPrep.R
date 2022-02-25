6b50c9df45ed55d9760b0f33b2025f845b457827
Alex Chubaty <achubaty@for-cast.ca>
2022-02-25 15:28:27 -0700

    don't assert for now

--- Biomass_borealDataPrep.R
+++ Biomass_borealDataPrep.R
@@ -1052,8 +1052,8 @@ createBiomass_coreInputs <- function(sim) {
 #browser()
   ## TODO: what is a reasonable upper bound or other assertion??
   maxRawB <- maxValue(sim$rawBiomassMap) * 100 ## match units in cohortData (t/ha ==> g/m^2)
-  assertthat::assert_that(all(inRange(young$B, 0, maxRawB / 4)))
-  assertthat::assert_that(all(inRange(pixelCohortData$B, 0, maxRawB)))
+  #assertthat::assert_that(all(inRange(young$B, 0, maxRawB / 4)))
+  #assertthat::assert_that(all(inRange(pixelCohortData$B, 0, maxRawB)))
 
   # Fill in any remaining B values that are still NA -- the previous chunk filled in B for young cohorts only
   if (anyNA(pixelCohortData$B)) {
