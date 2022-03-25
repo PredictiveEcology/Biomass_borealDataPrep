d9c6df7f90df3069a86b107cf35199862c09ced0
Ian Eddy <ianmseddy@gmail.com>
2022-03-25 23:03:03 +0000

    remove require false

--- R/updateYoungBiomasses.R
+++ R/updateYoungBiomasses.R
@@ -168,7 +168,7 @@ spinUpPartial <- function(pixelCohortData, speciesEcoregion, maxAge,
   if (!any(modules == "Biomass_core")) { # if Biomass_core doesn't exist in modulePath, then download it
     ## check that SpaDES.install is available in the right version
     if (!Require::Require("PredictiveEcology/SpaDES.install (>= 0.0.7)",
-                          require = FALSE, upgrade = FALSE, install = FALSE)) {
+                          upgrade = FALSE, install = FALSE)) {
       stop(paste("Please install SpaDES.install v0.0.7 or above using:",
                  "Require('PredictiveEcology/SpaDES.install (>= 0.0.7)', require = FALSE)"))
     }
