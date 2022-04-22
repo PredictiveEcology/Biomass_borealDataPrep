3dc26b4cd8158845cfeed8579c3ab4875c3d511e
CeresBarros <cbarros@mail.ubc.ca>
2022-04-22 10:04:21 -0700

    check rawBiomassMap and SALarge match before creating RTMLarge

--- Biomass_borealDataPrep.R
+++ Biomass_borealDataPrep.R
@@ -1393,6 +1393,15 @@ Save <- function(sim) {
     if (needRTML && !needRTM) {
       sim$rasterToMatchLarge <- sim$rasterToMatch
     } else if (needRTML && needRTM) {
+      if (!compareRaster(sim$rawBiomassMap, sim$studyAreaLarge, stopiffalse = FALSE)) {
+        ## note that extents may never align if the resolution and projection do not allow for it
+        sim$rawBiomassMap <- Cache(postProcessTerra,
+                                   sim$rawBiomassMap,
+                                   studyArea = sim$studyAreaLarge,
+                                   useSAcrs = TRUE,
+                                   overwrite = TRUE)
+        sim$rawBiomassMap <- fixErrors(sim$rawBiomassMap)
+      }
       sim$rasterToMatchLarge <- sim$rawBiomassMap
     }
 
