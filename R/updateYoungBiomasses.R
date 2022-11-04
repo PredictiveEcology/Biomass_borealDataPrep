aabb31d6b677394930280a28ae378ef9f5a43242
CeresBarros <cbarros@mail.ubc.ca>
2022-11-04 16:32:12 -0700

    spinUpPartial: use SpaDES.project, not SpaDES.install

--- R/updateYoungBiomasses.R
+++ R/updateYoungBiomasses.R
@@ -169,16 +169,16 @@ spinUpPartial <- function(pixelCohortData, speciesEcoregion, maxAge,
   if (!any(modules == "Biomass_core") ||
       moduleVersion("Biomass_core", paths$modulePath) < "1.3.5") { # if Biomass_core doesn't exist in modulePath or is too old, then download it
     ## check that SpaDES.install is available in the right version
-    if (!Require::Require("PredictiveEcology/SpaDES.install (>= 0.0.7)",
-                          upgrade = FALSE, install = FALSE)) {
-      stop(paste("Please install SpaDES.install v0.0.7 or above using:",
-                 "Require('PredictiveEcology/SpaDES.install (>= 0.0.7)', require = FALSE)"))
+    if (!Require::Require("PredictiveEcology/SpaDES.project (>= 0.0.7)",
+                          require = FALSE, upgrade = FALSE, install = FALSE)) {
+      stop(paste("Please install SpaDES.project v0.0.7 or above using:",
+                 "Require('PredictiveEcology/SpaDES.project (>= 0.0.7)', require = FALSE)"))
     }
 
     paths$modulePath <- file.path(curModPath, submodule, "module")
     moduleNameAndBranch <- c("Biomass_core@development (>= 1.3.9)")
     modules <- list(gsub("@.+", "", moduleNameAndBranch))
-    getModule(moduleNameAndBranch, modulePath = paths$modulePath, overwrite = TRUE) # will only overwrite if wrong version
+    SpaDES.project::getModule(moduleNameAndBranch, modulePath = paths$modulePath, overwrite = TRUE) # will only overwrite if wrong version
   } else {
   ## trim unnecessary modules:
   modules <- modules[modules == "Biomass_core"]
