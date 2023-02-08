2816105634b41b2037228c741b880e2745fc7bef
Eliot McIntire <eliotmcintire@gmail.com>
2023-02-08 11:50:26 -0800

    bump version

--- Biomass_borealDataPrep.R
+++ Biomass_borealDataPrep.R
@@ -9,7 +9,7 @@ defineModule(sim, list(
     person(c("Alex", "M."), "Chubaty", email = "achubaty@for-cast.ca", role = c("ctb"))
   ),
   childModules = character(0),
-  version = list(Biomass_borealDataPrep = "1.5.4"),
+  version = list(Biomass_borealDataPrep = "1.5.5"),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
