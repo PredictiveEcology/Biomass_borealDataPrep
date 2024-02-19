3ffe7b5d4ae1763312b7ca7ee3b81151e58c71a9
Eliot McIntire <eliotmcintire@gmail.com>
2024-02-19 10:43:50 -0800

    testing DESCRIPTION

---
+++ R/imports.R
@@ -0,0 +1,41 @@
+#' @import assertthat
+NULL
+
+#' @import dplyr
+NULL
+
+#' @import fasterize
+NULL
+
+#' @import ggplot2
+NULL
+
+#' @import plyr
+NULL
+
+
+#' @import rasterVis
+NULL
+
+#' @import sf
+NULL
+
+#' @import SpaDES.tools
+NULL
+
+#' @import terra
+NULL
+
+#' @import reproducible
+NULL
+
+
+#' @import SpaDES.core
+NULL
+
+#' @import SpaDES.project
+NULL
+
+#' @import pemisc
+NULL
+
