bc0c8baa75f21ee9922c3a8abe946eeece33ecc1
ianmseddy <ianmseddy@gmail.com>
2024-06-13 10:02:13 -0700

    fix plot typo?

--- R/plotFn_speciesEcoregion.R
+++ R/plotFn_speciesEcoregion.R
@@ -1,6 +1,6 @@
 plotFn_speciesEcoregion <- function(stk, SEtype) {
   ggSpeciesEcoregion <-
-    gplot(stk, maxpixels = 2e6) +
+    ggplot(stk, maxpixels = 2e6) +
     geom_tile(aes(fill = value)) +
     facet_wrap(~ variable) +
     scale_fill_distiller(palette = "YlGnBu", na.value = "white", direction = 1) +
